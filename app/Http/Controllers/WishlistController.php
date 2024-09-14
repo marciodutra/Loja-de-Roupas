d96714d81d3fafa7d5c0f0ffb7db609a732d126f
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-09-14 18:49:50 -0300

    Fazendo a lista de desejos

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -7,9 +7,27 @@ use Surfsidemedia\Shoppingcart\Facades\Cart;
 
 class WishlistController extends Controller
 {
+
+    public function index()
+    {
+        $items = Cart::instance('wishlist')->content();
+        return view('wishlist',compact('items'));
+    }
     public function add_to_wishlist(Request $request)
     {
       Cart::instance('wishlist')->add($request->id,$request->name,$request->quantity,$request->price)->associate('App\Models\Product');
       return redirect()->back();
+    }
+
+    public function remove_item($rowId)
+    {
+        Cart::instance('wishlist')->remove($rowId);
+        return redirect()->back();
+    }
+
+    public function empty_wishlist()
+    {
+        Cart::instance('wishlist')->destroy();
+        return redirect()->back();
     }
 }
---
+++ resources/views/wishlist.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+@section('content')
+<main class="pt-90">
+    <div class="mb-4 pb-4"></div>
+    <section class="shop-checkout container">
+      <h2 class="page-title">Produtos selecionados</h2>
+      
+      <div class="shopping-cart">
+        @if (Cart::instance('wishlist')->content()->count()>0)
+            
+        
+        <div class="cart-table__wrapper">
+          <table class="cart-table">
+            <thead>
+              <tr>
+                <th>Produto</th>
+                <th></th>
+                <th>Preço</th>
+                <th>Quantidade</th>
+                <th>Ação</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $item)
+                <tr>
+                    <td>
+                    <div class="shopping-cart__product-item">
+                        <img loading="lazy" src="{{asset('uploads/products/thumbnails')}}/{{$item->model->image}}" width="120" height="120" alt="{{$item->name}}" />
+                    </div>
+                    </td>
+                    <td>
+                    <div class="shopping-cart__product-item__detail">
+                        <h4>{{$item->name}}</h4>
+                        {{-- <ul class="shopping-cart__product-item__options">
+                        <li>Cor: Amarelo</li>
+                        <li>Tamanho: L</li>
+                        </ul> --}}
+                    </div>
+                    </td>
+                    <td>
+                    <span class="shopping-cart__product-price">R$ {{$item->price}}</span>
+                    </td>
+                    <td>
+                        {{$item->qty}}
+                    </td>                    
+                    <td>
+                        <form method="POST" action="{{route('wishlist.item.remove',['rowId'=>$item->rowId])}}" id="remove-item-{{$item->id}}">
+                            @csrf
+                            @method('DELETE')
+                            <a href="javascript:void(0)" class="remove-cart" onclick="document.getElementById('remove-item-{{$item->id}}').submit();">
+                                <svg width="10" height="10" viewBox="0 0 10 10" fill="#767676" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.259435 8.85506L9.11449 0L10 0.885506L1.14494 9.74056L0.259435 8.85506Z" />
+                                <path d="M0.885506 0.0889838L9.74057 8.94404L8.85506 9.82955L0 0.97449L0.885506 0.0889838Z" />
+                                </svg>
+                            </a>
+                        </form>
+                    </td>
+                </tr>
+              @endforeach 
+            </tbody>
+          </table>
+          <div class="cart-table-footer">
+            <form method="POST" action="{{route('wishlist.item.clear')}}">
+                @csrf
+                @method('DELETE')
+                 <button type="submit" class="btn btn-light">ESVAZIAR LISTA DE DESEJOS</button>
+            </form>
+          </div>
+        </div>
+        @else
+            <div class="row">
+                <div class="col-md-12">
+                    <p>Nenhum item encontrado em sua lista de desejos</p>
+                    <a href="{{route('shop.index')}}" class="btn btn-info">Compre agora</a>
+                </div>
+            </div>
+        @endif
+      </div>
+    </section>
+  </main>
+@endsection

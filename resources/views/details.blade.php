4527d04a46114bf760a0f445028fe37a819b4acc
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-09-14 16:19:10 -0300

    Finalizando os filtros

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('content')
-
+<style>
+   .filled-heart{
+    color: orange;
+   }
+</style>
 <main class="pt-90">
     <div class="mb-md-1 pb-md-3"></div>
     <section class="product-single container">
@@ -108,7 +112,7 @@
           </div>
           @if (Cart::instance('cart')->content()->where('id',$product->id)->count()>0)
               <a href="{{route('cart.index')}}" class="btn btn-warning mb-3">Vá para o carrinho</a>
-          @else            
+          @else
           <form name="addtocart-form" method="post" action="{{route('cart.add')}}">
             @csrf
             <div class="product-single__addtocart">
@@ -125,10 +129,26 @@
           </form>
           @endif
           <div class="product-single__addtolinks">
-            <a href="#" class="menu-link menu-link_us-s add-to-wishlist"><svg width="16" height="16" viewBox="0 0 20 20"
+            @if (Cart::instance('wishlist')->content()->where('id',$product->id)->count() > 0)
+            <a href="javascript:void(0)" class="menu-link menu-link_us-s add-to-wishlist filled-heart"><svg width="16" height="16" viewBox="0 0 20 20"
                 fill="none" xmlns="http://www.w3.org/2000/svg">
                 <use href="#icon_heart" />
-              </svg><span>Adicionar à lista de desejos</span></a>
+              </svg><span>Remover da lista de desejos</span></a>
+            @else
+            <form method="POST" action="{{route('wishlist.add')}}" id="wishlist-form">
+                @csrf
+                <input type="hidden" name="id" value="{{$product->id}}" />
+                <input type="hidden" name="name" value="{{$product->name}}" />
+                <input type="hidden" name="price" value="{{$product->sale_price == '' ? $product->regular_price : $product->sale_price}}" />
+                <input type="hidden" name="quantity" value="1" />
+                <a href="javascript:void(0)" class="menu-link menu-link_us-s add-to-wishlist" onclick="document.getElementById('wishlist-form').submit();"><svg width="16" height="16" viewBox="0 0 20 20"
+                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <use href="#icon_heart" />
+                </svg><span>Adicionar a lista de desejos</span></a>
+            </form>
+            @endif
+
+
             <share-button class="share-button">
               <button class="menu-link menu-link_us-s to-share border-0 bg-transparent d-flex align-items-center">
                 <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">

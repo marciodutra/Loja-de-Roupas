863a5148fc7053880354b92d49b41dee4ab9f2e2
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-09-22 15:28:06 -0300

    Finalizando dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,11 +11,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-shopping-bag"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Total de pedidos</div>
-                                    <h4>3</h4>
+                                    <h4>{{$dashboardDatas[0]->Total}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -26,11 +26,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-dollar-sign"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Total</div>
-                                    <h4>R$ 481.34</h4>
+                                    <h4>R$ {{$dashboardDatas[0]->TotalAmount}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -41,11 +41,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-shopping-bag"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Pedidos pendentes</div>
-                                    <h4>3</h4>
+                                    <h4>{{$dashboardDatas[0]->TotalOrdered}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -56,11 +56,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-dollar-sign"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Total Pedidos pendentes</div>
-                                    <h4>R$ 481.34</h4>
+                                    <h4>R$ {{$dashboardDatas[0]->TotalOrderedAmount}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -74,11 +74,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-shopping-bag"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Pedidos entregues</div>
-                                    <h4>0</h4>
+                                    <h4>{{$dashboardDatas[0]->TotalDelivered}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -89,11 +89,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-dollar-sign"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Total de pedidos entregues</div>
-                                    <h4>R$ 0.00</h4>
+                                    <h4>R$ {{$dashboardDatas[0]->TotalDeliveredAmount}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -104,11 +104,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-shopping-bag"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Pedidos cancelados</div>
-                                    <h4>0</h4>
+                                    <h4>{{$dashboardDatas[0]->TotalCanceled}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -119,11 +119,11 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center gap14">
                                 <div class="image ic-bg">
-                                    <i class="icon-dollar-sign"></i>
+                                    <i></i>
                                 </div>
                                 <div>
                                     <div class="body-text mb-2">Total de Pedidos cancelados</div>
-                                    <h4>R$ 0.00</h4>
+                                    <h4>R$ {{$dashboardDatas[0]->TotalCanceledAmount}}</h4>
                                 </div>
                             </div>
                         </div>
@@ -135,52 +135,51 @@
 
             <div class="wg-box">
                 <div class="flex items-center justify-between">
-                    <h5>Receita de ganhos</h5>
-                    <div class="dropdown default">
-                        <button class="btn btn-secondary dropdown-toggle" type="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            <span class="icon-more"><i class="icon-more-horizontal"></i></span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li>
-                                <a href="javascript:void(0);">Essa semana</a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0);">Semana passada</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <h5>Receita mensal</h5>                    
                 </div>
                 <div class="flex flex-wrap gap40">
                     <div>
                         <div class="mb-2">
                             <div class="block-legend">
                                 <div class="dot t1"></div>
-                                <div class="text-tiny">Receita</div>
+                                <div class="text-tiny">Total</div>
                             </div>
                         </div>
                         <div class="flex items-center gap10">
-                            <h4>R$37,80</h4>
-                            <div class="box-icon-trending up">
-                                <i class="icon-trending-up"></i>
-                                <div class="body-title number">0.56%</div>
-                            </div>
+                            <h4>R$ {{$TotalAmount}}</h4>                            
                         </div>
                     </div>
                     <div>
                         <div class="mb-2">
                             <div class="block-legend">
                                 <div class="dot t2"></div>
-                                <div class="text-tiny">Pedido</div>
+                                <div class="text-tiny">Pendente</div>
                             </div>
                         </div>
                         <div class="flex items-center gap10">
-                            <h4>R$28,305</h4>
-                            <div class="box-icon-trending up">
-                                <i class="icon-trending-up"></i>
-                                <div class="body-title number">0.56%</div>
+                            <h4>R$ {{$TotalOrderedAmount}}</h4>                            
+                        </div>
+                    </div>
+                    <div>
+                        <div class="mb-2">
+                            <div class="block-legend">
+                                <div class="dot t2"></div>
+                                <div class="text-tiny">Entregue</div>
                             </div>
+                        </div>
+                        <div class="flex items-center gap10">
+                            <h4>R$ {{$TotalDeliveredAmount}}</h4>                            
+                        </div>
+                    </div>
+                    <div>
+                        <div class="mb-2">
+                            <div class="block-legend">
+                                <div class="dot t2"></div>
+                                <div class="text-tiny">Cancelado</div>
+                            </div>
+                        </div>
+                        <div class="flex items-center gap10">
+                            <h4>R$ {{$TotalCanceledAmount}}</h4>                            
                         </div>
                     </div>
                 </div>
@@ -194,7 +193,7 @@
                 <div class="flex items-center justify-between">
                     <h5>Pedidos recentes</h5>
                     <div class="dropdown default">
-                        <a class="btn btn-secondary dropdown-toggle" href="#">
+                        <a class="btn btn-secondary dropdown-toggle" href="{{route('admin.orders')}}">
                             <span class="view-all">Ver todos</span>
                         </a>
                     </div>
@@ -204,35 +203,42 @@
                         <table class="table table-striped table-bordered">
                             <thead>
                                 <tr>
-                                    <th style="width: 80px">Nº pedido</th>
-                                    <th>Nome</th>
+                                    <th style="width:70px">Nº do pedido</th>
+                                    <th class="text-center">Nome</th>
                                     <th class="text-center">Telefone</th>
                                     <th class="text-center">Subtotal</th>
                                     <th class="text-center">Tax</th>
                                     <th class="text-center">Total</th>
-
                                     <th class="text-center">Status</th>
                                     <th class="text-center">Data do pedido</th>
-                                    <th class="text-center">Total Items</th>
+                                    <th class="text-center">Total de Itens</th>
                                     <th class="text-center">Entregue em</th>
                                     <th></th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @foreach ($orders as $order)
                                 <tr>
-                                    <td class="text-center">1</td>
-                                    <td class="text-center">Divyansh Kumar</td>
-                                    <td class="text-center">51 33521456</td>
-                                    <td class="text-center">R$172.00</td>
-                                    <td class="text-center">R$36.12</td>
-                                    <td class="text-center">R$208.12</td>
-
-                                    <td class="text-center">Encomendado</td>
-                                    <td class="text-center">2024-07-11 00:54:14</td>
-                                    <td class="text-center">2</td>
-                                    <td></td>
+                                    <td class="text-center">{{$order->id}}</td>
+                                    <td class="text-center">{{{$order->name}}}</td>
+                                    <td class="text-center">{{$order->phone}}</td></td>
+                                    <td class="text-center">R$ {{$order->subtotal}}</td>
+                                    <td class="text-center">R$ {{$order->tax}}</td>
+                                    <td class="text-center">R$ {{$order->total}}</td>
                                     <td class="text-center">
-                                        <a href="#">
+                                        @if($order->status == 'delivered')
+                                            <span class="badge bg-success">Entregue</span>
+                                        @elseif($order->status == 'canceled')
+                                            <span class="badge bg-danger">Cancelado</span>
+                                        @else
+                                            <span class="badge bg-warning">Encomendado</span>
+                                        @endif
+                                    </td>
+                                    <td class="text-center">{{$order->created_at}}</td>
+                                    <td class="text-center">{{$order->orderItems->count()}}</td>
+                                    <td class="text-center">{{$order->delivered_date}}</td>
+                                    <td class="text-center">
+                                        <a href="{{route('admin.order.details',['order_id'=>$order->id])}}">
                                             <div class="list-icon-function view-icon">
                                                 <div class="item eye">
                                                     <i class="icon-eye"></i>
@@ -241,6 +247,7 @@
                                         </a>
                                     </td>
                                 </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -252,3 +259,104 @@
 
 </div>
 @endsection
+
+@push('scripts')
+<script>
+    (function ($) {
+
+        var tfLineChart = (function () {
+
+            var chartBar = function () {
+
+                var options = {
+                    series: [{
+                        name: 'Total',
+                        data: [{{$AmountM}}]
+                    }, {
+                        name: 'Pending',
+                        data: [{{$OrderedAmountM}}]
+                    },
+                    {
+                        name: 'Delivered',
+                        data: [{{$DeliveredAmountM}}]
+                    }, {
+                        name: 'Canceled',
+                        data: [{{$CanceledAmountM}}]
+                    }],
+                    chart: {
+                        type: 'bar',
+                        height: 325,
+                        toolbar: {
+                            show: false,
+                        },
+                    },
+                    plotOptions: {
+                        bar: {
+                            horizontal: false,
+                            columnWidth: '10px',
+                            endingShape: 'rounded'
+                        },
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    legend: {
+                        show: false,
+                    },
+                    colors: ['#2377FC', '#FFA500', '#078407', '#FF0000'],
+                    stroke: {
+                        show: false,
+                    },
+                    xaxis: {
+                        labels: {
+                            style: {
+                                colors: '#212529',
+                            },
+                        },
+                        categories: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                    },
+                    yaxis: {
+                        show: false,
+                    },
+                    fill: {
+                        opacity: 1
+                    },
+                    tooltip: {
+                        y: {
+                            formatter: function (val) {
+                                return "$ " + val + ""
+                            }
+                        }
+                    }
+                };
+
+                chart = new ApexCharts(
+                    document.querySelector("#line-chart-8"),
+                    options
+                );
+                if ($("#line-chart-8").length > 0) {
+                    chart.render();
+                }
+            };
+
+            /* Function ============ */
+            return {
+                init: function () { },
+
+                load: function () {
+                    chartBar();
+                },
+                resize: function () { },
+            };
+        })();
+
+        jQuery(document).ready(function () { });
+
+        jQuery(window).on("load", function () {
+            tfLineChart.load();
+        });
+
+        jQuery(window).on("resize", function () { });
+    })(jQuery);
+</script>
+@endpush
---
+++ resources/views/admin/slide-add.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.admin')
+@section('content')
+<div class="main-content-inner">
+    <!-- main-content-wrap -->
+    <div class="main-content-wrap">
+        <div class="flex items-center flex-wrap justify-between gap20 mb-27">
+            <h3>Controle deslizante</h3>
+            <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
+                <li>
+                    <a href="{{route('admin.index')}}">
+                        <div class="text-tiny">Dashboard</div>
+                    </a>
+                </li>
+                <li>
+                    <i class="icon-chevron-right"></i>
+                </li>
+                <li>
+                    <a href="{{route('admin.slide.add')}}">
+                        <div class="text-tiny">Controle deslizante</div>
+                    </a>
+                </li>
+                <li>
+                    <i class="icon-chevron-right"></i>
+                </li>
+                <li>
+                    <div class="text-tiny">Novo controle deslizante</div>
+                </li>
+            </ul>
+        </div>        
+        <div class="wg-box">
+            <form class="form-new-product form-style-1" action="{{route('admin.slide.store')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <fieldset class="name">
+                    <div class="body-title">Slogan <span class="tf-color-1">*</span></div>
+                    <input class="flex-grow" type="text" placeholder="Slogan" name="tagline" tabindex="0" value="{{old('tagline')}}" aria-required="true" required="">
+                </fieldset>
+                @error('tagline') <span class="alert alert-danger text-center">{{$message}}</span> @enderror
+                <fieldset class="name">
+                    <div class="body-title">Título <span class="tf-color-1">*</span></div>
+                    <input class="flex-grow" type="text" placeholder="Título" name="title" tabindex="0" value="{{old('title')}}" aria-required="true" required="">
+                </fieldset>
+                @error('title') <span class="alert alert-danger text-center">{{$message}}</span> @enderror
+                <fieldset class="name">
+                    <div class="body-title">Subtitulo <span class="tf-color-1">*</span></div>
+                    <input class="flex-grow" type="text" placeholder="Subtitulo" name="subtitle" tabindex="0" value="{{old('subtitle')}}" aria-required="true" required="">
+                </fieldset>
+                @error('subtitle') <span class="alert alert-danger text-center">{{$message}}</span> @enderror                        
+                <fieldset>
+                    <div class="body-title">Carregar imagens <span class="tf-color-1">*</span>
+                    </div>
+                    <div class="upload-image flex-grow">
+                        <div class="item" id="imgpreview" style="display:none;">
+                            <img src="sample.jpg" class="effect8" alt=""/>
+                        </div>
+                        <div class="item up-load">
+                            <label class="uploadfile" for="myFile">
+                                <span class="icon">
+                                    <i class="icon-upload-cloud"></i>
+                                </span>
+                                <span class="body-text">Solte suas imagens aqui ou selecione <span class="tf-color">Clique para navegar</span></span>
+                                <input type="file" id="myFile" name="image">
+                            </label>
+                        </div>
+                    </div>
+                </fieldset>
+                @error('image') <span class="alert alert-danger text-center">{{$message}}</span> @enderror
+                <fieldset class="category">
+                    <div class="body-title">Status</div>
+                    <div class="select flex-grow">
+                        <select class="" name="status">
+                            <option>Selecione</option>
+                            <option value="1" @if (old('status')=="1") selected @endif>Ativo</option>
+                            <option value="0" @if (old('status')=="0") selected @endif>Inativo</option>
+                        </select>
+                    </div>
+                </fieldset>
+                @error('status') <span class="alert alert-danger text-center">{{$message}}</span> @enderror
+                <div class="bot">
+                    <div></div>
+                    <button class="tf-button w208" type="submit">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
+@push('scripts')
+    <script>
+        $(function(){
+            $("#myFile").on("change",function(e){
+                const photoInp = $("#myFile");
+                const [file] = this.files;
+                if(file)
+            {
+                $("#imgpreview img").attr('src',URL.createObjectURL(file));
+                $("#imgpreview").show();
+            }
+            });            
+        });        
+    </script>
+@endpush
